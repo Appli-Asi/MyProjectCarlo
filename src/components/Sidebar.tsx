03e9dfce2d9f54eb29340d581117d3586ccd8fd4
John Carlo Asi <asijo@brothergroup.net>
2026-07-30 16:59:08 +0800

    add sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h2>Menu</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Sidebar;
